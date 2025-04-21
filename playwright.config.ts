b323c494182e1565e27ef25d45f09dcf1b5bfe21
ARIKBOGA <you@example.com>
2025-04-21 16:30:56 +0300

    Update Playwright configuration to include action and navigation timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 90 * 1000, // 90 saniye
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -30,6 +31,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+     // Her bir adımın (örn. locator.click, locator.getByText) timeout süresi
+     actionTimeout: 30 * 1000, // örneğin 30 saniye
+     navigationTimeout: 60 * 1000,
   },
 
   /* Configure projects for major browsers */
