9068fcf83a6e29302453b47177d18a91803a1de4
ARIKBOGA <barikboga42@gmail.com>
2025-04-27 18:52:59 +0300

    Add global setup for Playwright tests and create LoginAuth.json for session storage

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -12,8 +12,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  globalSetup: "./storage/globalSetup.ts",
   timeout: 90 * 1000, // 90 saniye
-  testDir: './tests',
+  testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -23,24 +24,25 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-     // Her bir adımın (örn. locator.click, locator.getByText) timeout süresi
-     actionTimeout: 10 * 1000, // örneğin 30 saniye
-     navigationTimeout: 60 * 1000,
+    trace: "on-first-retry",
+    // Her bir adımın (örn. locator.click, locator.getByText) timeout süresi
+    actionTimeout: 10 * 1000, // örneğin 30 saniye
+    navigationTimeout: 60 * 1000,
+    storageState: "storage/LoginAuth.json" // Oturum açma bilgilerini içeren dosya
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     // {
