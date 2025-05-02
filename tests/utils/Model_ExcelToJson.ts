d9b97f7fc1b0a8245523a9a5376cf94a39c14437
ARIKBOGA <barikboga42@gmail.com>
2025-05-02 21:16:47 +0300

    Add utilities for converting Excel files to JSON format

---
+++ tests/utils/Model_ExcelToJson.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import path from "path";
+import * as XLSX from "xlsx";
+
+const filePathToRead = path.resolve(__dirname, '../../data/katalogInfo/excels/model_seri_no.xlsx');
+const filePathToWrite = path.resolve(__dirname, '../../data/katalogInfo/jsons/model_seri_no.json');
+
+const convertToArrayOfObjectsJSON = (filePath: string) => {
+    // Çalışma kitabını dosyadan okuyun
+    const workbook = XLSX.readFile(filePath, { cellDates: true });
+
+    // İlk sayfayı alın
+    const firstSheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[firstSheetName];
+
+    // Sayfayı JSON'a dönüştürün, başlık satırını anahtar olarak kullanın
+    const jsonData: any[] = XLSX.utils.sheet_to_json(worksheet);
+
+    return jsonData;
+};
+
+// Excel dosyasını nesne dizisi formatında JSON'a dönüştürün
+const arrayOfObjectsJsonData = convertToArrayOfObjectsJSON(filePathToRead);
+
+// JSON verilerini bir dosyaya yazın
+fs.writeFile(filePathToWrite, JSON.stringify(arrayOfObjectsJsonData, null, 2), (err) => {
+    if (err) {
+        console.error('JSON dosyası yazılırken hata oluştu:', err);
+    } else {
+        console.log('JSON dosyası kaydedildi.');
+    }
+});
+
+console.log('Excel dosyası nesne dizisi formatında JSON\'a dönüştürüldü:', arrayOfObjectsJsonData);
