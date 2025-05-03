17aaf7c165f6789103665f7d59f861ef30d5498a
ARIKBOGA <barikboga42@gmail.com>
2025-05-04 02:04:51 +0300

    feat: Add selectors for Application Scraper

---
+++ src/utils/Selectors.ts
@@ -0,0 +1,8 @@
+export const selector = {
+    // Application Scraper
+    aria_level_1_brand: '//*[@aria-level="1"]',
+    aria_level_2_vehicle: '//*[@aria-level="2"]/div[1]',
+    aria_level_3_rows: '//tbody//tr[@role="row"]',
+    cells_part_1: '//tbody//tr[@role="row"][',
+    cells_part_2: ']//saam-table-cell'
+}
