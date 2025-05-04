f14cc41b35581da9d94c90c3e3c16b4ea53d8ec3
ARIKBOGA <barikboga42@gmail.com>
2025-05-04 23:56:51 +0300

    fix: Update getEnvVariable method to return an empty string instead of undefined

--- src/utils/ConfigReader.ts
+++ src/utils/ConfigReader.ts
@@ -2,19 +2,21 @@ import * as fs from "fs";
 import * as path from "path";
 
 class ConfigReader {
-    private static envFilePath = path.resolve(__dirname, "../data/Configs/.env");
+  private static envFilePath = path.resolve(__dirname, "../data/Configs/.env");
 
-    // .env dosyasından bir değişkeni oku
-    static getEnvVariable(key: string): string | undefined {
-        try {
-            const envContent = fs.readFileSync(this.envFilePath, "utf8");
-            const line = envContent.split("\n").find(line => line.startsWith(`${key}=`));
-            return line ? line.split("=")[1].trim() : undefined;
-        } catch (error) {
-            console.error(`Error reading .env file: ${error}`);
-            return undefined;
-        }
+  // .env dosyasından bir değişkeni oku
+  static getEnvVariable(key: string): string {
+    try {
+      const envContent = fs.readFileSync(this.envFilePath, "utf8");
+      const line = envContent
+        .split("\n")
+        .find((line) => line.startsWith(`${key}=`));
+      return line ? line.split("=")[1].trim() : "";
+    } catch (error) {
+      console.error(`Error reading .env file: ${error}`);
+      return "";
     }
+  }
 }
 
 export default ConfigReader;
