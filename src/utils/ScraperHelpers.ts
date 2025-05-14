eeabfa51f61450dc469d4bfd11f8407ed8c8d7d5
ARIKBOGA <you@example.com>
2025-05-14 18:01:43 +0300

    Add goToSearchResultsEnglish function for English RepXpert page navigation and product link retrieval

--- src/utils/ScraperHelpers.ts
+++ src/utils/ScraperHelpers.ts
@@ -36,6 +36,43 @@ export async function goToSearchResults(
   return productLinks; // bulunan ürünleri geri dön
 }
 
+export async function goToSearchResultsEnglish(
+  page: Page,
+  oe: string,
+  filterBrand: string,
+  retryList: string[],
+  addToRetryList: (oe: string) => void
+): Promise<Locator[] | null> {
+
+  // İngilizce RepXpert sayfasına git
+  await page.goto(ConfigReader.getEnvVariable("REPXPERT_ENGLISH_URL") || "");
+  await page.getByRole("textbox", { name: /OE number/i }).fill(oe);
+  await page.getByRole("textbox", { name: /OE number/i }).press("Enter");
+
+  await page
+    .getByRole("combobox", { name: /Brands/i })
+    .fill(filterBrand.toLowerCase() || "");
+  await page
+    .getByRole("checkbox", { name: new RegExp(filterBrand, "i") })
+    .first()
+    .click();
+  await page.waitForTimeout(2000);
+
+  const productLinks = await page
+    .getByRole("link", { name: new RegExp(filterBrand, "i") })
+    .all();
+
+  if (productLinks.length === 0) {
+    console.warn(`⚠️ '${oe}' için ${filterBrand} ürünü bulunamadı.`);
+    if (!retryList.includes(oe)) {
+      addToRetryList(oe);
+    }
+    return null; // hiçbir ürün yoksa null dön
+  }
+
+  return productLinks; // bulunan ürünleri geri dön
+}
+
 export async function loginEnglishRepxpertPage(page: Page) {
   const url = ConfigReader.getEnvVariable("REPXPERT_ENGLISH_URL");
   const email = ConfigReader.getEnvVariable("REPXPERT_ENGLISH_EMAIL");
