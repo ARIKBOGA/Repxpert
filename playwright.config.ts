1e3c7f44bb206dc8a5d4c591d36f745f22fe1ab2
ARIKBOGA <barikboga42@gmail.com>
2025-05-24 14:58:55 +0300

    refactor: uncomment dotenv configuration in playwright setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,9 +6,8 @@ import path from "path";
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
