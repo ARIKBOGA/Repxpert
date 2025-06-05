e6cc426fedabbdb229ce72c81209029c92d797a4
ARIKBOGA <barikboga42@gmail.com>
2025-06-05 14:57:38 +0300

    Add aria_level_2_vehicleBox selector to Application Scraper

--- src/utils/Selectors.ts
+++ src/utils/Selectors.ts
@@ -1,6 +1,7 @@
 export const selector = {
     // Application Scraper
     aria_level_1_brand: '//*[@aria-level="1"]',
+    aria_level_2_vehicleBox: '(//*[@aria-level="2"])',
     aria_level_2_vehicle: '//*[@aria-level="2"]/div[1]',
     aria_level_3_rows: '//tbody//tr[@role="row"]',
     cells_part_1: '//tbody//tr[@role="row"][',
