2b03cd2b9b4770e7764848b381f8d245ae32f1ba
ARIKBOGA <you@example.com>
2025-06-18 13:40:23 +0300

    Refactor ECA_Code_Generator for improved structure and number generation logic

--- src/Singular/ECA_Code_Generator.ts
+++ src/Singular/ECA_Code_Generator.ts
@@ -1,53 +1,41 @@
+import * as fs from "fs";
+import * as path from "path";
 
-import * as fs from 'fs';
-import * as path from 'path';
-
-
-const brand_initials: string[] = Array.from({ length: 990 }, (_, i) => String(i + 1));
+const brand_initials: string[] = Array.from({ length: 291 }, (_, i) => String(i + 10));
 
 const drum_range: string[] = Array.from({ length: 408 }, (_, i) => String(i + 1093));
-const passengerCar_disc_range: string[] = Array.from({ length: 1500 }, (_, i) => String(i + 1500));
-const passengerCar_pad_range: string[] = Array.from({ length: 100 }, (_, i) => String(i + 20000));
+const passengerCar_disc_range: string[] = Array.from({ length: 1499 }, (_, i) => String(i + 1501));
+const discBrakePad_range: string[] = Array.from({ length: 28000 }, (_, i) => String(i + 10000));
+//const drumBrakePad_range: string[] = Array.from({ length: 10000 }, (_, i) => String(i + 10000));
+//const clutchFacingsPad_range: string[] = Array.from({ length: 5000 }, (_, i) => String(i + 30000));
+//const frictionPlatesPad_range: string[] = Array.from({ length: 3000 }, (_, i) => String(i + 35000));
 
-const disc_additions: string[] = ["","B", "C", "CS", "H", "S"];
+const disc_additions: string[] = ["", "B", "C", "CS", "H", "S"];
 const drum_additions: string[] = ["", "H"];
 const pad_centers: string[] = Array.from({ length: 9 }, (_, i) => String(i + 1));
-const pad_additions: string[] = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15"];
+const pad_additions: string[] = [ "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15"];
 
-function getDrumNumbers(): string[] {
-    return brand_initials.flatMap(initial =>
-        drum_range.flatMap(drum =>
-            drum_additions.map(drum_addition => initial + drum + drum_addition)
-        )
-    );
-}
 
-function getDiscNumbers(): string[] {
-    return brand_initials.flatMap(initial =>
-        passengerCar_disc_range.flatMap(disc =>
-            disc_additions.map(disc_addition => initial + disc + disc_addition)
-        )
-    );
-}
-
-function getPadnumbers(): string[] {
-    return brand_initials.flatMap(initial =>
-        passengerCar_pad_range.flatMap(pad =>
-            pad_centers.flatMap(pad_center =>
-                pad_additions.map(pad_addition => initial + pad + pad_center + pad_addition)
-            )
-        )
-    );
+function numberGenerator(initial: string[], centers: string[], additions: string[]): string[] {
+    return initial.flatMap((initial) =>
+        centers.flatMap((center) =>
+        additions.map((addition) => initial + center + addition)
+      )
+  );
 }
 
 function main() {
-    const drumNumbers = getDrumNumbers();
-    //const discNumbers = getDiscNumbers();
-    //const padNumbers = getPadnumbers();
 
-    const drumNumbersPath = path.resolve("src/data/Produced/ECA", "drumNumbers.json");
-    fs.writeFileSync(drumNumbersPath, JSON.stringify(drumNumbers, null, 2), 'utf-8');
+  const drumNumbers = numberGenerator(brand_initials, drum_range, drum_additions);
+  const discNumbers = numberGenerator(brand_initials, passengerCar_disc_range, disc_additions);
+  const padNumbers = numberGenerator(discBrakePad_range, pad_centers, pad_additions);
+
+  const OUTPUT_PATH = path.join(`src/data/Produced/ECA/`);
+
+  fs.writeFileSync(`${OUTPUT_PATH}drumNumbers.json`, JSON.stringify(drumNumbers, null, 2), "utf-8");
+  fs.writeFileSync(`${OUTPUT_PATH}discNumbers.json`, JSON.stringify(discNumbers, null, 2), "utf-8");
+  fs.writeFileSync(`${OUTPUT_PATH}padNumbers.json`, JSON.stringify(padNumbers, null, 2), "utf-8");
+
 }
-
 
 main();
