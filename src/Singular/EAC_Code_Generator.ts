1a0d07b8fc6a5c9cbf470927e8c67a3bdfba9561
ARIKBOGA <you@example.com>
2025-06-20 14:10:05 +0300

    Fix output path in EAC_Code_Generator to correct folder name and clean up formatting

--- src/Singular/EAC_Code_Generator.ts
+++ src/Singular/EAC_Code_Generator.ts
@@ -14,9 +14,9 @@ const pad_centers: string[] = Array.from({ length: 9 }, (_, i) => String(i + 1))
 const pad_additions: string[] = Array.from({ length: 15 }, (_, i) => String(i + 1).padStart(2, '0'));
 
 function numberGenerator(initial: string[], centers: string[], additions: string[]): string[] {
-  return  initial.flatMap((initial) =>
-          centers.flatMap((center) =>
-          additions.map((addition) => initial + center + addition)
+  return initial.flatMap((initial) =>
+    centers.flatMap((center) =>
+      additions.map((addition) => initial + center + addition)
     )
   );
 }
@@ -26,31 +26,31 @@ function main() {
 
   const disc_drum_yvCodes = numberGenerator(barcode_initials, disc_drum_yvno_range, disc_drum_additions);
   const pad_yvCodes = numberGenerator(wva_range, pad_centers, pad_additions);
-  
+
   // Create an array of objects that contain the file name, numbers and column count to be used when generating the Excel file
   const data: { fileName: string; numbers: string[]; columnCount: number; }[] = [
-    { fileName: "DISC_DRUM", numbers: disc_drum_yvCodes, columnCount: 6},
-    { fileName: "PAD", numbers: pad_yvCodes, columnCount: 15},
+    { fileName: "DISC_DRUM", numbers: disc_drum_yvCodes, columnCount: 6 },
+    { fileName: "PAD", numbers: pad_yvCodes, columnCount: 15 },
   ];
-  
+
   const wb = XLSX.utils.book_new();
-  const OUTPUT_PATH = path.join(`src/data/Produced/ECA/`);
+  const OUTPUT_PATH = path.join(`src/data/Produced/EAC/`);
 
   // Iterate over the data array and create a worksheet for each object
   data.forEach((element) => {
+
     const { fileName, numbers, columnCount } = element;
-    
     const ws_data: string[][] = [];
-    
+
     // Iterate over the numbers array and create chunks of columnCount length
     for (let i = 0; i < numbers.length; i += columnCount) {
       ws_data.push(numbers.slice(i, i + columnCount)); // [1, columnCount], [columnCount+1, 2*columnCount], ...
     }
-    
+
     // Create the worksheet from the worksheet data
     const ws = XLSX.utils.aoa_to_sheet(ws_data);
-    XLSX.utils.book_append_sheet(wb, ws, fileName);  
-    
+    XLSX.utils.book_append_sheet(wb, ws, fileName);
+
     fs.writeFileSync(`${OUTPUT_PATH}${fileName}.json`, JSON.stringify(numbers, null, 2), "utf-8");
   });
 
@@ -58,7 +58,7 @@ function main() {
   XLSX.writeFile(wb, `${OUTPUT_PATH}${outputFileName}`);
 
   console.log(`✨ Excel dosyası oluşturuldu: ${OUTPUT_PATH}${outputFileName}`);
-  
+
 }
 
 main();
