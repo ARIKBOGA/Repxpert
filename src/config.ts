1eee512372cff092ef0537001d93923b3467620d
ARIKBOGA <you@example.com>
2025-06-25 15:35:42 +0300

    Update configuration for English application paths and global setup

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const PRODUCT_TYPE = process.env.PRODUCT_TYPE as string;
 export const FILTER_BRAND_APPLICATION = process.env.FILTER_BRAND_APPLICATION as string;
 
 // Sabit dosya yolları
-export const ROOT_PATH_APPLICATIONS = `src/data/Gathered_Informations/${PRODUCT_TYPE}/Applications/${FILTER_BRAND_APPLICATION}`;
+export const ROOT_PATH_APPLICATIONS = `src/data/Gathered_Informations/${PRODUCT_TYPE}/Applications/English/${FILTER_BRAND_APPLICATION}`;
 export const MARKA_FILE_PATH = "src/data/katalogInfo/jsons/marka_tur.json";
 export const MODEL_FILE_PATH = "src/data/katalogInfo/jsons/model_tur.json";
 export const LOOKUP_EXCEL_FILE_PATH = `src/data/katalogInfo/excels/${PRODUCT_TYPE}_katalog_full.xlsx`;
